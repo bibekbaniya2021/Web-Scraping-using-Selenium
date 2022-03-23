b0f2fbe2cbdaf3957cf5709f74cd006f693174b2
codebibek <97448890+bibekbaniya2021@users.noreply.github.com>
2022-03-23 16:46:39 +0000

    Added function to get videos

--- scraper.py
+++ scraper.py
@@ -1,15 +1,34 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By
 
-YOUTUBE_TRENDING_URL = 'https://www.youtube.com/feed/trending'
+YOUTUBE_TRENDING_URL = 'https://www.youtube.com/results?search_query=web+scraping'
+# 'https://www.youtube.com/feed/trending'
 
-chrome_options = Options()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-dev-shm-usage')
+def get_driver():
+  chrome_options = Options()
+  chrome_options.add_argument('--no-sandbox')
+  chrome_options.add_argument('--headless')
+  chrome_options.add_argument('--disable-dev-shm-usage')
+  driver = webdriver.Chrome(options=chrome_options)
+  return driver
 
-driver = webdriver.Chrome(options=chrome_options)
-driver.get(YOUTUBE_TRENDING_URL)
+def get_videos():
+  VIDEO_DIV_TAG = 'ytd-video-renderer'
+  driver.get(YOUTUBE_TRENDING_URL)
+  videos = driver.find_elements(By.TAG_NAME, VIDEO_DIV_TAG)
+  return videos  
+   
 
-print('Page title:', driver.title)
+if __name__ == "__main__":
+  print('Creating driver')
+  driver = get_driver()
+
+  print('fetching list of Top site for search query "web scraping" ')
+  video = get_videos(driver)
+  # print('Page title:', driver.title)
+  
+  print(f'found {len(video)} videos')
+  
+  
+  
